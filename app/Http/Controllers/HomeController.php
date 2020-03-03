5611b201b86af227df143312a1ce3138802b0d87
darkheart101 <kurokokoro@windowslive.com>
2020-03-03 20:15:30 +0200

    chore: news api key

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,17 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $client = new Client([
+            // Base URI is used with relative requests
+            'base_uri' => 'http://newsapi.org/v2/',
+            // You can set any number of default request options.
+            'timeout'  => 2.0,
+        ]);
+
+        $response = $client->request('GET', 'top-headlines?country=us&apiKey='.config('app.news_api_key'));
+        $response = json_decode($response->getBody());
+        dd($response);
+
         return view('home');
     }
 }
