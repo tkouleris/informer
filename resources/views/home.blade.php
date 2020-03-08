20803c0a50de5f8d773cb3d75ceef91fb4934601
darkheart101 <kurokokoro@windowslive.com>
2020-03-08 20:46:27 +0200

    feature: search bar placeholder

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">
+                    <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+                    <i class="fas fa-search" aria-hidden="true"></i>
+                </div>
 
                 <div class="card-body">
                     @if (session('status'))
