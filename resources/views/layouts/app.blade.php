86ae0a4aae19a993f5c8a008f9456365a118fac8
darkheart101 <kurokokoro@windowslive.com>
2020-05-23 19:35:00 +0300

    fix: load javascript

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,11 @@
     <!-- Scripts -->
     <script src="{{ asset('js/constants.js') }}" defer></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="{{ asset('js/settings.js') }}" defer></script>
-    <script src="{{ asset('js/newsfeed.js') }}" defer></script>
+    @auth
+        <script src="{{ asset('js/settings.js') }}" defer></script>
+        <script src="{{ asset('js/newsfeed.js') }}" defer></script>
+    @endauth
+
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
