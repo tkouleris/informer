efaa8096be52498342f0da6c37087fb026b72444
darkheart101 <kurokokoro@windowslive.com>
2020-07-12 13:14:05 +0300

    feature: newsfeed api

--- app/Http/Controllers/ApiControllers/NewsfeedController.php
+++ app/Http/Controllers/ApiControllers/NewsfeedController.php
@@ -2,26 +2,43 @@
 
 namespace App\Http\Controllers\ApiControllers;
 
+use App\Services\SettingsPageService;
 use App\Services\UserNewsService;
 use Illuminate\Http\Request;
 
 class NewsfeedController extends Controller
 {
     protected $userNewsService;
+    protected $settingsPageService;
+    protected $items_per_page = 5;
 
     /**
      * NewsfeedController constructor.
-     * @param $userNewsService
+     * @param UserNewsService $userNewsService
+     * @param SettingsPageService $settingsPageService
      */
-    public function __construct(UserNewsService $userNewsService)
+    public function __construct(UserNewsService $userNewsService, SettingsPageService $settingsPageService)
     {
+        parent::__construct();
         $this->userNewsService = $userNewsService;
+        $this->settingsPageService = $settingsPageService;
     }
 
 
-    public function feed()
+    public function feed(Request $request)
     {
         $user = auth()->user();
-        return $this->userNewsService->fetch($user->id);
+        $search_query = $request->input('search_query');
+        $category_filter = $request->input('category');
+        $articles = $this->userNewsService->fetch($user->id);
+        $categories = $this->settingsPageService->fetch_active_categories($user->id);
+        $page = 1;
+        if( isset( $request->page ) )
+        {
+            $page = $request->page;
+        }
+        $total_pages = $articles->count()/$this->items_per_page;
+        $articles = $articles->forPage($page,$this->items_per_page);
+        return $articles;
     }
 }
